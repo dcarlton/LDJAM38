7871e18446a9f4279e5d4545b11c2c931707a4c4
Dustin Carlton <dustin.carlton.is@gmail.com>
2017-04-21 21:42:36 -0500

    Creating a window.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,55 @@
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
 #include <windows.h>
 
 #include "SDL.h"
 
+namespace
+{
+    std::ofstream logFile;
+}
+
+void logInfo(char* info)
+{
+    std::ofstream logFile;
+    logFile.open("log.txt", std::ios::out | std::ios::app);
+    logFile << info << "\n";
+    logFile.close();
+    logFile.clear();
+}
+
 int main()
 {
+    std::remove("log.txt");
+
+    SDL_Log("Initializing SDL");
+    SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
+    SDL_Log("Finished initializing SDL");
+
+    // TODO: Better title
+    SDL_Window* window = SDL_CreateWindow("A Small World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                          160, 192, 0);
+
+    /*SDL_Event test_event;
+    while (true)
+    {
+        while (SDL_PollEvent(&test_event))
+        {
+            if (test_event.type == SDL_KEYDOWN && test_event.key.keysym.sym == SDLK_ESCAPE)
+            {
+                SDL_Quit();
+                return 0;
+            }
+        }
+
+        SDL_UpdateWindowSurface(window);
+    }*/
+
+    SDL_Log("Shutting down");
+    SDL_Quit();
+    SDL_Log("Smell ya later!");
     return 0;
 }
 
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-cl main.cpp /I SDL2-2.0.5\include /link /libpath SDL2-2.0.5\lib\x86\SDL2.lib
-main.exe
+cl /EHsc main.cpp /I SDL2-2.0.5\include /link /libpath SDL2-2.0.5\lib\x86\SDL2.lib
+main.exe | cat
 del main.exe
