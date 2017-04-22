1a9f5d5aab7445044714a785860c918e7440d61d
Dustin Carlton <dustin.carlton.is@gmail.com>
2017-04-21 20:16:21 -0500

    Compiling with SDL

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
+#include <windows.h>
+
+#include "SDL.h"
+
 int main()
 {
     return 0;
+}
+
+// VS Windows applications need WinMain as an entry point
+int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    return main();
 }
--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
-cl main.cpp
+@echo off
+cl main.cpp /I SDL2-2.0.5\include /link /libpath SDL2-2.0.5\lib\x86\SDL2.lib
 main.exe
 del main.exe
