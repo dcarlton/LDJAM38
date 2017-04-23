a835e5d374d9615b177c2ba371524f942506d597
Dustin Carlton <dustin.carlton.is@gmail.com>
2017-04-23 16:52:06 -0500

    Renamed the executable.

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
 cl /EHsc main.cpp /I SDL2-2.0.5\include /I SDL2_ttf-2.0.14\include /I SDL2_mixer-2.0.1\include /link /libpath SDL2-2.0.5\lib\x86\SDL2.lib /libpath SDL2_ttf-2.0.14\lib\x86\SDL2_ttf.lib /libpath SDL2_mixer-2.0.1\lib\x86\SDL2_mixer.lib
-main.exe | cat
-del main.exe
+ren main.exe TinyDungeon.exe
+TinyDungeon.exe | cat
+del TinyDungeon.exe
