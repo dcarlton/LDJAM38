52c248bbf4a451600eb6269ff1618a5f47ea922e
Dustin Carlton <dustin.carlton.is@gmail.com>
2017-04-23 17:04:21 -0500

    Renamed the main source file.

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
-cl /EHsc main.cpp /I lib\SDL2-2.0.5\include /I lib\SDL2_ttf-2.0.14\include /I lib\SDL2_mixer-2.0.1\include /link /libpath lib\SDL2-2.0.5\lib\x86\SDL2.lib /libpath lib\SDL2_ttf-2.0.14\lib\x86\SDL2_ttf.lib /libpath lib\SDL2_mixer-2.0.1\lib\x86\SDL2_mixer.lib
-ren main.exe TinyDungeon.exe
+cl /EHsc TinyDungeon.cpp /I lib\SDL2-2.0.5\include /I lib\SDL2_ttf-2.0.14\include /I lib\SDL2_mixer-2.0.1\include /link /libpath lib\SDL2-2.0.5\lib\x86\SDL2.lib /libpath lib\SDL2_ttf-2.0.14\lib\x86\SDL2_ttf.lib /libpath lib\SDL2_mixer-2.0.1\lib\x86\SDL2_mixer.lib
 TinyDungeon.exe | cat
 del TinyDungeon.exe
